90bce60545226c476f2ab1dc5de1ff842988e9dd
Tony Holmes <5856798+mplssign@users.noreply.github.com>
2026-01-23 19:38:01 -0600

    fix: make banner more defensive - only initialize on mobile web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6be7539d00d979a66896968399625e6c",
+const RESOURCES = {"flutter_bootstrap.js": "7602e08ccc7b912f526e620d55bd1b6b",
 "version.json": "7d868deefe5b70e7d6f326256262ed48",
 "bandroadie_horiz.png": "4f9f69a5cba63553490b2d4c64a9ae8b",
 "index.html": "c2641473f3ecbdf8f0a9d56318954f3d",
 "/": "c2641473f3ecbdf8f0a9d56318954f3d",
 "firebase-messaging-sw.js": "406c31f9d237c56c8c1fc8ceb75b6b5d",
 "support.html": "a9a4b6f8216bfba4a17db941cc37bdd9",
-"main.dart.js": "34b3e75852f22bde580d35d85b3337de",
+"main.dart.js": "32c9a963f5d08b16a3eb6ee48ae56f2b",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "351dca6f2e725ba85c34ba62323c9214",
 "icons/Icon-192.png": "db652681fa5504c0a0027bbf8a3a2329",
