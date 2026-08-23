30ce00e206e4797665b7d9fae23133f6db2d4b2a
Tony Holmes <tonycraig@gmail.com>
2026-08-23 09:19:47 -0500

    test: intentional test failure for CI negative test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,4 +4,8 @@ void main() {
   test('dummy test', () {
     expect(1 + 1, 2);
   });
+
+  test('intentional failure for CI negative test', () {
+    expect(1, equals(2)); // This will fail
+  });
 }
